648892e0f51a25a7b8e2e07fb16c23a2df810368
pduchesne <pduchesne@gmail.com>
2016-09-09 18:53:52 +0200

    mov env definition from app to server

---
+++ src/main/resources/env.js
@@ -0,0 +1,18 @@
+(function (window) {
+    window.__env = window.__env || {};
+
+
+    window.__env.auth_providers = [
+        {
+            "provider" : "google",
+            "clientId" : "456026922964-9sjvu7cubkd9nb1d5u2r8gnvm8p70om8.apps.googleusercontent.com"
+        },
+        {
+            "provider" : "facebook",
+            "clientId" : "540932659407006"
+        }
+    ];
+
+    window.__env.apiUrl = '/api';
+    window.__env.enableDebug = true;
+}(this));
